dd1496103b0d24a15d081d03391d82eec0b51215
Johannes Fürmann <johannes@weltraumpflege.org>
2014-01-28 16:06:36 +0100

    Integrate get_fingerprints.py
    * Enables tor control port in default config
    * enhances get_fingerprints.py

--- tools/get_fingerprints.py
+++ tools/get_fingerprints.py
@@ -1,11 +1,22 @@
+from os import listdir
+from os.path import join
+
 from stem.control import Controller
 
 def main():
-    with Controller.from_port(port=9051) as controller:
-        controller.authenticate() 
-        fingerprint = controller.get_info("fingerprint")
+    stmt_path = '/var/run/torctl/statements'
+    stmt_files = [ x for x in listdir(stmt_path) if 'stmt' in x ]
+    for stmt_file in stmt_files:
+        with open(join(stmt_path,stmt_file)) as f:
+            stmt = f.read()
+            address = stmt.split("=")[1]
+            port=int(address.split(":")[1])
 
-        print fingerprint
-  
+            with Controller.from_port(port=port) as controller:
+                controller.authenticate() 
+                fingerprint = controller.get_info("fingerprint")
+
+                print fingerprint
+    
 if __name__ == "__main__":
     main()
